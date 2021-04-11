ce502811bc2fa839b67a13eada17134a3d860afc
niksonnet <nikpatel10@live.com>
2021-04-11 11:52:46 +0530

    Vue initial code added

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <ul>
+    <li v-for="item in storedResources" :key="item.id"></li>
+  </ul>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'R-01',
+          title: 'Offical Vue.js Guide',
+          description: 'Offical Vue.js Guide documentation',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'R-02',
+          title: 'Google',
+          description: 'Learn google!',
+          link: 'https://google.com',
+        },
+      ],
+    };
+  },
+};
+</script>
